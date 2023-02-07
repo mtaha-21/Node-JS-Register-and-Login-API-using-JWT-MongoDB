7c99ecef765f165f3017b5d6b005eda29aeebe97
Taha <iamtahamurtaza@gmail.com>
2023-02-07 13:38:03 +0530

    Working on auth and error middleware

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dbConfig = require("./config/db.config");
+
+const auth = require("./middlewares/auth");
+const errors = require("./middlewares/errors");
+
+const unless = require("express-unless");
+
+const app = express();
+
+mongoose
+  .connect(dbConfig.db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => {
+      console.log("Database connection established");
+    },
+    (error) => {
+      console.log("Database can't be connected" + error);
+    }
+  );
+
+auth.authenticateToken.unless = unless;
+app.use(
+  auth.authenticateToken.unless({
+    path: [
+      { url: "/users/login", methods: ["POST"] },
+      { url: "/users/register", methods: ["POST"] },
+    ],
+  })
+);
+
+app.use(express.json());
+
+app.use("/users", require("./routes/users.routes"));
+
+app.use(errors.errorHandler);
+
+app.listen(process.env.port || 4000, function () {
+  console.log("Ready to go");
+});
